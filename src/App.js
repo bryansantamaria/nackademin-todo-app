92794ce94805275ee816fb0bae38453b946c46af
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-19 18:50:18 +0200

    added a public folder with index.html and a  src folder for react scripts

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This page will contain some awesome react later.</p>
+        <p> But first I will focus on creating the BACKEND! :D</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
