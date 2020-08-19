d69ae0e0cfdc72314f6c09bfdaa84dbcb2f1908b
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-19 21:25:55 +0200

    added a /create route for post, using addToDo from toDoController

--- models/toDoModel.js
+++ models/toDoModel.js
@@ -1,13 +1,13 @@
-const Datastore = require('nedb-promises'),
-    const toDoCollection = new Datastore({
-        filename: "./database/ToDo.db",
-        autoload: true,
-    });
+const Datastore = require("nedb-promises");
+const toDoCollection = new Datastore({
+  filename: "./database/ToDo.db",
+  autoload: true,
+});
 
 const createToDo = async (title, done) => {
-    const doc = await toDoCollection.insert({title, done});
+  const doc = await toDoCollection.insert({ title, done });
 
-    return doc;
-}
+  return doc;
+};
 
-module.exports = {createToDo}
+module.exports = { createToDo };
--- routes/toDoRoute.js
+++ routes/toDoRoute.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const { addToDo } = require("../controllers/toDoController");
 
 const router = new Router();
+
+router.post("/create", addToDo);
 
 module.exports = router;
