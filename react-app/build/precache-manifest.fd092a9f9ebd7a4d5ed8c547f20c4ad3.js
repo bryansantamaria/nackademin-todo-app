95bf8788cd9c6159e6aaeaf40e8d98b8f0156f81
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-20 09:33:22 +0200

    set up react application with starting scripts and using express.static to run server on same port as react

---
+++ react-app/build/precache-manifest.fd092a9f9ebd7a4d5ed8c547f20c4ad3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecb60c3021a539a753696e81c508373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "983ce8dd78f2a30fce76",
+    "url": "/static/js/2.0b7b6522.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b7b6522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d10caec0340c0fae2d1",
+    "url": "/static/js/main.2a41ba02.chunk.js"
+  },
+  {
+    "revision": "31f2f9333b920a76bae7",
+    "url": "/static/js/runtime-main.2ee42059.js"
+  }
+]);
---
+++ react-app/server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const router = require("./routes/toDoRoute.js");
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use("/todo", router);
+
+app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
