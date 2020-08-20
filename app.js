30d8fedc776c0eed0d60ffdb57a3eb8b9ed5d237
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-20 07:52:57 +0200

    added __dirname public

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express();
 const PORT = 8080;
 
 app.use(express.json());
+app.use(express.static((__dirname, "public")));
 
-app.use("/", router);
+app.use("/todo", router);
 
 app.listen(PORT, () => {
   console.log(`Listening to port: ${PORT}`);
