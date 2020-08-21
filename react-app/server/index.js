3fcf3064b0c51bad8798cd2bbc76a759b8bedea8
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-21 16:05:15 +0200

    Added styling with material ui components, update method have logic to update specific item.

--- react-app/server/index.js
+++ react-app/server/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 const router = require("./routes/toDoRoute.js");
+const cors = require("cors");
 
 const app = express();
 const PORT = 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use("/todo", router);
 
