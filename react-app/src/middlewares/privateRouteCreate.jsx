a7de483141ca5c83627e1e2840e5ae1e04673460
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-08-30 16:35:55 +0200

    added middlewares folder, privateRoutes and privateRoute create from my other repo

---
+++ react-app/src/middlewares/privateRouteCreate.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+
+function PrivateRouteCreate({ component: Component, ...rest }) {
+  const isAdmin = localStorage.getItem("role");
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAdmin === "admin" ? (
+          <Component exact path="/todo" {...props} {...rest} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+}
+
+export default PrivateRouteCreate;
