953d04e3906c7603e3d09a266f8711789fa52006
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-05 10:20:17 +0200

    Create Todo-list works from frontend to backend, and GET list also works with a selected compontent

--- react-app/server/start.js
+++ react-app/server/start.js
@@ -1,6 +1,6 @@
-const app = require("./index");
+const app = require('./index');
 
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log(`Listening to port: ${PORT}`);
+	console.log(`Listening to port: ${PORT}`);
 });
