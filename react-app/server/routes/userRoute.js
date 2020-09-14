71569ab9ca9f43d20eef90f508fcbbf2beb5883c
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-14 09:27:42 +0200

    updated userRoutes for deleting and getting userData

--- react-app/server/routes/userRoute.js
+++ react-app/server/routes/userRoute.js
@@ -1,9 +1,12 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { create, login, getUser } = require("../controllers/userController");
-const { authenticate } = require("../middlewares/auth");
+const { create, login, getUser, userData, deleteUser } = require('../controllers/userController');
+const { authenticate } = require('../middlewares/auth');
 
-router.post("/create", authenticate, create);
-router.post("/auth", login);
-router.get("/", authenticate, getUser);
+router.post('/create', authenticate, create);
+router.post('/auth', login);
+router.get('/getData', authenticate, userData);
+router.get('/delete', authenticate, deleteUser);
+router.get('/', authenticate, getUser);
+
 module.exports = router;
