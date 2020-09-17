e0263795f1b12f7f6cafa56e169c5a478ee4c53a
Bryan Santamaria <bryaan.santamaria@gmail.com>
2020-09-17 09:47:36 +0200

    removed developement db

--- server/database/dataBase.js
+++ server/database/dataBase.js
@@ -5,9 +5,9 @@ let mongoDatabase;
 
 switch (process.env.ENVIRONMENT) {
 	case 'development':
-		uri = `mongodb://localhost:27017`;
-		mongoose.connect(uri);
-		break;
+	// uri = `mongodb://localhost:27017`;
+	// mongoose.connect(uri);
+	// break;
 	case 'test':
 		const { MongoMemoryServer } = require('mongodb-memory-server');
 		mongoDatabase = new MongoMemoryServer();
